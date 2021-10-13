bb783d3ea1f8109f19c68b6f74eb4bfd10a477b7
Shibui Yusuke <yusuke.shibui@npc2001007.local>
2021-10-13 15:52:39 +0900

    update

--- src/model/model.py
+++ src/model/model.py
@@ -1,8 +1,10 @@
-from typing import Union
+from typing import Optional, Union
+from copy import deepcopy
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from lightgbm import LGBMClassifier
 
 
 class PreprocessPipeline(object):
@@ -29,4 +31,15 @@ class RandomForestClassifierPipeline(object):
         self,
         preprocess_pipeline: Pipeline,
     ):
-        self.preprocess_pipeline = preprocess_pipeline
+        self.__preprocess_pipeline = preprocess_pipeline
+        self.random_forest_classifier: RandomForestClassifier = RandomForestClassifier()
+        self.pipeline = deepcopy(self.__preprocess_pipeline)
+        self.pipeline.steps.append(("model", self.random_forest_classifier))
+
+    def define_random_forest_classifier(
+        self,
+        **random_forest_classifier_params,
+    ):
+        self.random_forest_classifier = RandomForestClassifier(**random_forest_classifier_params)
+        self.pipeline = deepcopy(self.__preprocess_pipeline)
+        self.pipeline.steps.append(("model", self.random_forest_classifier))
